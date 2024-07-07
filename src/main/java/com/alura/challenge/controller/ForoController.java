87c7dd3d0d4ac337ac660b3b1a5ac43e8a95cc68
roanbaga <roanbaga@gmail.com>
2024-07-06 23:18:25 -0500

    Prueba

--- src/main/java/com/alura/challenge/controller/ForoController.java
+++ src/main/java/com/alura/challenge/controller/ForoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/temas")
 public class ForoController{
-
+//* probando
     @PostMapping
     public void registarTema(@RequestBody DatoRegistroTema datoRegistroTema ){
 
